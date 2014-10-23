85b9cf8f66f333e1df17c81e5288b6e08654edde
sajeewau <sajeewau@gmail.com>
2014-10-23 13:47:09 +0530

    Database Server Changed

--- LatestERPAdvantage/ERPSolution/ERPAdvantage/Login/LogMeIn.aspx.cs
+++ LatestERPAdvantage/ERPSolution/ERPAdvantage/Login/LogMeIn.aspx.cs
@@ -66,7 +66,7 @@ namespace ERPAdvantage.Login
             {
             if (success == true)
             {
-               Response.Redirect("~/MST/CustomerMaster.aspx");
+               Response.Redirect("~/Default.aspx");
             }
             }
 
