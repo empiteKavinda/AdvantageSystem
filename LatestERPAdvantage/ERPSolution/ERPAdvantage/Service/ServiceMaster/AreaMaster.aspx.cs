124b7b6ff2b41a8e9b9ea6ac642577a14a3ccd41
Kavinda <dilii386xp@gmail.com>
2014-10-27 15:01:27 +0530

    AreaMaster.aspx.cs Cutomer Cervise Center and Area Dropdown Create

--- LatestERPAdvantage/ERPSolution/ERPAdvantage/Service/ServiceMaster/AreaMaster.aspx.cs
+++ LatestERPAdvantage/ERPSolution/ERPAdvantage/Service/ServiceMaster/AreaMaster.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Advantage.ERP.BLL;
+using Advantage.ERP.DAL.DataContract;
 
 namespace ERPAdvantage.Service.ServiceMaster
 {
@@ -11,7 +13,33 @@ namespace ERPAdvantage.Service.ServiceMaster
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                GetArea();
+                GetCustomerServiceCenter();
+            }
+        }
 
+        private void GetArea()
+        {
+            var uicon = new UIControl();
+            var wsoj = new ADTWebService();
+            var objMst = new CustomMaster
+            {
+                pOrgCode = ERPSystemData.COM_DOM_ORG_CODE.AEL.ToString(),
+                pDomType = ERPSystemData.COM_DOM_TYPE.AREA.ToString()
+            };
+            List<gDropdownlist> drplist = wsoj.pMsGetCategory(objMst);
+            uicon.FillDropdownList(ddlArea, drplist, "COM_DOM_CODE", "COM_DOM_DESC");
+        }
+
+        private void GetCustomerServiceCenter()
+        {
+            var uicon = new UIControl();
+            var wsoj = new ADTWebService();
+            var orgCode = ERPSystemData.COM_DOM_ORG_CODE.AEL.ToString();
+            var drplist = wsoj.PMsGetCustomerServiceCenter(orgCode);
+            uicon.FillDropdownList(ddlCutomerServiceCenter, drplist, "COM_DOM_CODE", "COM_DOM_DESC");
         }
     }
 }
